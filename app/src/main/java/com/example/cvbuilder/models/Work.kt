1bf4866ab6f923ca99b087f34647418131db4e55
felipe-zg <felipe_zeba@outlook.com>
2022-12-11 21:46:02 -0600

    feat: add work model

---
+++ app/src/main/java/com/example/cvbuilder/models/Work.kt
@@ -0,0 +1,3 @@
+package com.example.cvbuilder.models
+
+data class Work(var title: String, var description: String, var company: String, var client: String, var startDate: String, var endDate: String, var image: Int): java.io.Serializable
